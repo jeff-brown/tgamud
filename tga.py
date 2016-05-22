b1a8d7e4b276eade78d91af2a80a1a8d7098446c
Jeff Brown <jeffbr@Jeffs-MacBook-Pro.local>
2016-05-21 17:14:12 -0700

    fourth commit

--- tga.py
+++ tga.py
@@ -15,13 +15,31 @@ import urllib, re
 from time import sleep
 from subprocess import Popen, PIPE
 import time
+from Queue import Queue
+from threading import Thread
 
 DEBUG = True
 
+def do_stuff(q):
+  while True:
+    print q.get(),
+    q.task_done()
+
 if __name__ == '__main__':
+    '''
     if len(argv) < 2:
         print "usage %s" % (argv[0])
-                              
-    #
-    #executes after all metrics
-    # 
+    '''
+ 
+    q = Queue(maxsize=0)
+    num_threads = 10
+     
+    for i in range(num_threads):
+      worker = Thread(target=do_stuff, args=(q,))
+      worker.setDaemon(DEBUG)
+      worker.start()
+     
+    for x in range(100):
+      q.put(x)
+     
+    q.join()
