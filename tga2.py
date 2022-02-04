2c1c8afc23d259e5f8b2d3c1bc7043c49f85dc70
Jeffrey Brown <jeffreybrown@Jeffreys-MacBook-Pro-3.local>
2022-02-03 20:49:26 -0800

    clean up formatting and add docstrings

--- tga2.py
+++ tga2.py
@@ -1,25 +1,60 @@
-#!/usr/bin/python -uROO
+#!/usr/bin/env python
+"""
+ File Name : tga2.py
+
+ This script will demonstrate a simple multi-threaded game loop.
+
+ Creation date : Thu Feb  3 20:43:03 PST 2022
+ Last update : Thu Feb  3 20:43:03 PST 2022
+
+ Created By : Jeff Bronw <jeffbr@gmail.com>
+ Updated By : Jeff Brown <jeffbr@gmail.com>
+
+ Project Goals:
+    1) Demonstrate a basic multi-threaded game lookup
+    2) Implement a remote access mutli-user dungeon
+    3) Use ruleset based on D&D5E
+"""
 
 import time
 import Queue as queue
 import threading
 
-def console(q):
+
+def console(cmd_q):
+    """
+    collect user input from terminal and push it to a queue
+
+    args:
+        cmd_q (queue): the queue
+
+    returns:
+        None
+    """
     while True:
-        q.put(raw_input('> '))
-        
-def business(q):
+        cmd_q.put(input('> '))
+
+
+def business(cmd_q):
+    """
+    does something in the background and then goes to sleep
+
+    args:
+        cmd_q (queue): the queue
+
+    returns:
+        None
+    """
     while True:
-        q.put("Business, man!")
+        cmd_q.put("Business, man!")
         time.sleep(15)
 
+
 if __name__ == '__main__':
-    
     cmd_queue = queue.Queue()
 
     threading.Thread(target=console, args=(cmd_queue,)).start()
     threading.Thread(target=business, args=(cmd_queue,)).start()
 
     while True:
-        print cmd_queue.get()
-        
+        print(cmd_queue.get())
