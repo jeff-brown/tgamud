b34ccc5ec68627fbea1e8c30d5c1480435ebc329
Jeffrey Brown <jeffreybrown@Jeffreys-MBP-2.localdomain>
2022-02-12 23:52:51 -0800

    nightly checkin

--- smud.py
+++ smud.py
@@ -31,6 +31,7 @@ Game Loop:
 author: Mark Frimston - mfrimston@gmail.com
 """
 
+import random
 import sys
 import time
 
@@ -125,11 +126,166 @@ class Game():
             }
         ]
 
-        self._monsters = {}
+        self._mm = [
+            {  # 0
+                "name": "cave bear",
+                "long": "The cave bear has dark brown fur and stands well over \
+                         seven feet tall at the shoulder. Its teeth and claws \
+                         gleam like daggers as it sizes you up.",
+                "room": [4, 3]
+            },
+            {  # 1
+                "name": "huge rat",
+                "long": "The huge rat resembles rats you've seen before, \
+                         except that it is about two feet tall at the \
+                         shoulder, and seems much more aggressive.",
+                "room": [1, 1]
+            },
+            {  # 2
+                "name": "imp",
+                "long": "The imp is a tiny humanoid with pointed ears and \
+                         bright red skin. It stands just over two feet in \
+                         height and is armed with a dagger.",
+                "room": [1, 1]
+            },
+            {
+                "name": "female kobold",
+                "long": "The kobold is a small humanoid with doglike facial \
+                         features and is covered with coarse body hair. She \
+                         stands just over three feet in height, is wearing a \
+                         filthy tunic, and is armed with {small one-handed}.",
+                "room": [1, 1]
+            },
+            {
+                "name": "kobold",
+                "long": "The kobold is a small humanoid with doglike facial \
+                         features and is covered with coarse body hair. He \
+                         stands just under four feet in height, is wearing a \
+                         filthy tunic, and is armed with {small one-handed}.",
+                "room": [1, 1]
+            },
+            {
+                "name": "female orc",
+                "long": "The orc is a smallish humanoid with piglike facial \
+                         features and is covered sparsely by coarse body hair. \
+                         She stands just over four feet in height, is wearing \
+                         a leather tunic, and is armed with club.",
+                "room": [1, 1]
+            },
+            {
+                "name": "orc",
+                "long": "The orc is a smallish humanoid with piglike facial \
+                         features and is covered sparsely by coarse body hair. \
+                         He stands just under five feet in height, is wearing \
+                         a leather cuirass, and is armed with club.",
+                "room": [1, 1]
+            },
+            {
+                "name": "skeleton warrior",
+                "long": "The skeleton warrior is wearing tattered armor and \
+                         mouldering bits of old clothing, and is armed with \
+                         {large two-handed} {1}.",
+                "room": [1, 1]
+            },
+            {
+                "name": "giant bat",
+                "long": "The giant bat has a wingspan of over twelve feet and \
+                         has wicked looking claws and teeth.",
+                "room": [1, 1]
+            },
+            {
+                "name": "lizard woman",
+                "long": "The lizard woman is a five foot tall bipedal \
+                         humanoid who's features resemble those of a large \
+                         lizard. She has greyish scaley skin, and sharp claws \
+                         and teeth. The lizard woman is armed with scimitar.",
+                "room": [1, 1]
+            },
+            {
+                "name": "lizard man",
+                "long": "The lizard man is a six foot tall bipedal humanoid \
+                         who's features resemble those of a large lizard. He \
+                         has greenish scaley skin, and sharp claws and teeth. \
+                         The lizard man is armed with scimitar.",
+                "room": [1, 1]
+            },
+            {
+                "name": "female hobgoblin",
+                "long": "The hobgoblin is a squat apelike humanoid with dark \
+                         skin and pointed ears. She stands just under five \
+                         feet tall, is wearing a leather cuirass, and is armed \
+                         with small one-handed.",
+                "room": [1, 1]
+            },
+            {
+                "name": "hobgoblin",
+                "long": "The hobgoblin is a squat apelike humanoid with dark \
+                         skin and pointed ears. He stands just over five feet \
+                         tall, is wearing a leather cuirass, and is armed \
+                         with {small one-handed} {1}.",
+                "room": [1, 1]
+            },
+            {
+                "name": "cave bear",
+                "long": "The cave bear has dark brown fur and stands well \
+                         over seven feet tall at the shoulder. Its teeth and \
+                         claws gleam like daggers as it sizes you up.",
+                "room": [1, 1]
+            },
+            {
+                "name": "female cyclops",
+                "long": "The cyclops resembles a very large woman with only \
+                         one eye in the center of her forehead. She stands \
+                         over eleven feet tall, and is armed with a spiked \
+                         club.",
+                "room": [1, 1]
+            },
+            {
+                "name": "cyclops",
+                "long": "The cyclops resembles a very large man with only one \
+                         eye in the center of his forehead. He stands over \
+                         twelve feet tall, and is armed with a spiked club.",
+                "room": [1, 1]
+            },
+            {
+                "name": "female minotaur",
+                "long": "The minotaur has the upper torso of a muscular woman,\
+                         and the head, legs, and hooves of a bull. She is \
+                         wearing ringmail armor, and is armed with large \
+                         one-handed.",
+                "room": [1, 1]
+            },
+            {
+                "name": "minotaur",
+                "long": "The minotaur has the upper torso of a muscular man, \
+                         and the head, legs, and hooves of a bull. He is \
+                         wearing ringmail armor, and is armed with large \
+                         one-handed.",
+                "room": [1, 1]
+            },
+            {
+                "name": "ogress",
+                "long": "The ogress resembles an enormous and very ugly woman. \
+                         She stands over seven feet tall and is very muscular. \
+                         Her clothing is filthy and poorly made. She is armed \
+                         with {large two-handed} {1}.",
+                "room": [1, 1]
+            },
+            {
+                "name": "ogre",
+                "long": "The ogre resembles an enormous and very ugly man. \
+                         He stands over eight feet tall and is very muscular. \
+                         His clothing is filthy and poorly made. He is armed \
+                         with {0} {1}.",
+                "room": [1, 1]
+            }
+        ]
 
         self._mud = mud
 
         self._players = {}
+
+        self._monsters = {}
 
         self._commands = [
             "look", "north", "south", "east", "west", "help", "say"
@@ -142,11 +298,13 @@ class Game():
         exits = []
         room = None
 
-        x_coord = self._players[uid]["grid"][0]
-        y_coord = self._players[uid]["grid"][1]
+        x_coord = self._players[uid]["room"][0]
+        y_coord = self._players[uid]["room"][1]
 
         room = self._grid[x_coord][y_coord]
-
+        print("{}'s current loc [{},{}] in {}.".format(
+            self._players[uid]["name"],
+            x_coord, y_coord, self._rooms[room]["short"]))
         if self._grid[x_coord - 1][y_coord] > 0:
             exits.append("north")
         if self._grid[x_coord + 1][y_coord] > 0:
@@ -165,15 +323,20 @@ class Game():
         room, exits = self._movement(uid)
 
         players_here = []
+        monsters_here = []
         # go through every player in the game
         for pid, player in self._players.items():
             # if they're in the same room as the player
-            if player["grid"] == self._players[uid]["grid"] and \
+            if player["room"] == self._players[uid]["room"] and \
                     pid != uid:
                 # ... and they have a name to be shown
                 if player["name"] is not None:
                     # add their name to the list
                     players_here.append(player["name"])
+
+        for _, monster in self._monsters.items():
+            if monster["room"] == self._players[uid]["room"]:
+                monsters_here.append(monster["name"])
 
         # send player a message containing the list of players in the room
         if players_here:
@@ -181,8 +344,20 @@ class Game():
         else:
             who = "There is nobody here."
 
+        if monsters_here:
+            if len(monsters_here) > 1:
+                monster_line = (
+                    "There is a {} here".format(", and a ".join(
+                        monsters_here)))
+            else:
+                monster_line = "There is a {} here.".format(monsters_here[0])
+
         self._mud.send_message(uid, self._rooms[room]["short"])
-        self._mud.send_message(uid, who)
+        if monsters_here:
+            self._mud.send_message(uid, monster_line)
+        if players_here and monsters_here or not players_here \
+                and not monsters_here:
+            self._mud.send_message(uid, who)
         self._mud.send_message(uid, "You can go {}.".format(", ".join(exits)))
 
     def _process_help_command(self, uid):
@@ -204,7 +379,9 @@ class Game():
         add a new players name to the dictionary and stick them in a room
         """
         self._players[uid]["name"] = command
-        self._players[uid]["grid"] = [4, 2]
+        self._players[uid]["room"] = [4, 2]
+        self._players[uid]["fatigue"] = time.time()
+        print(self._players)
 
         # go through all the players in the game
         for pid, _ in self._players.items():
@@ -226,7 +403,7 @@ class Game():
         """
         for pid, player in self._players.items():
             # if they're in the same room as the player
-            if player["grid"] == self._players[uid]["grid"] \
+            if player["room"] == self._players[uid]["room"] \
                     and pid != uid:
                 # send them a message telling them what the player said
                 self._mud.send_message(pid, "{} says: {}".format(
@@ -258,7 +435,7 @@ class Game():
             for pid, player in self._players.items():
                 # if player is in the same room and isn't the player
                 # sending the command
-                if player["grid"] == self._players[uid]["grid"] \
+                if player["room"] == self._players[uid]["room"] \
                         and pid != uid:
                     # send them a message telling them that the player
                     # left the room
@@ -268,19 +445,19 @@ class Game():
 
             # update the player's current room to the one the exit leads to
             if door == "south":
-                self._players[uid]["grid"][0] += 1
+                self._players[uid]["room"][0] += 1
             if door == "north":
-                self._players[uid]["grid"][0] -= 1
+                self._players[uid]["room"][0] -= 1
             if door == "east":
-                self._players[uid]["grid"][1] += 1
+                self._players[uid]["room"][1] += 1
             if door == "west":
-                self._players[uid]["grid"][1] -= 1
+                self._players[uid]["room"][1] -= 1
 
             # go through all the players in the game
             for pid, player in self._players.items():
                 # if player is in the same (new) room and isn't the player
                 # sending the command
-                if player["grid"] == self._players[uid]["grid"] \
+                if player["room"] == self._players[uid]["room"] \
                         and pid != uid:
                     # send them a message telling them that the player
                     # entered the room
@@ -297,6 +474,28 @@ class Game():
             self._mud.send_message(
                 uid, "A mysterious force blocks your path to the {}.".format(
                     door))
+
+    def _process_attack_command(self, uid, params):
+        """
+        try to kill some stuff why not
+        """
+        for mid, monster in self._monsters.items():
+            if params in monster["name"]:
+                if time.time() - self._players[uid]["fatigue"] > 15:
+                    self._mud.send_message(
+                        uid, "You attack {}.".format(monster["name"]))
+                    del self._monsters[mid]
+                    self._players[uid]["fatigue"] = time.time()
+                else:
+                    self._mud.send_message(
+                        uid, (
+                            "You are still physically exhausted from your "
+                            "previous activities!"
+                        )
+                    )
+            else:
+                self._mud.send_message(
+                    uid, "Sorry, you don't see '{}' nearby.".format(params))
 
     def check_for_new_players(self):
         """
@@ -381,6 +580,12 @@ class Game():
                 # go to another rooms
                 self._process_go_command(uid, params)
 
+            # "attack" command
+            elif command in ["attack", "a"]:
+
+                # let's gooooo
+                self._process_attack_command(uid, params)
+
             # 'exit' command
             elif command == "quit":
 
@@ -392,6 +597,18 @@ class Game():
                 # send back an 'unknown command' message
                 self._mud.send_message(
                     uid, "Unknown command '{}'".format(command))
+
+    def check_for_monsters(self):
+        """
+        spawn monsters and move them around
+        """
+
+        # spawn rnd monster in arena if empty
+        if not self._monsters:
+            self._monsters[0] = random.choice(self._mm)
+            self._monsters[0]["room"] = [4, 3]
+            self._monsters[0]["fatigue"] = time.time()
+            print("spawned {}".format(self._monsters[0]["name"]))
 
 
 def main():
@@ -417,6 +634,8 @@ def main():
         # 'update' must be called in the loop to keep the game running and give
         # us up-to-date information
         mud.update()
+
+        game.check_for_monsters()
 
         game.check_for_new_players()
 
